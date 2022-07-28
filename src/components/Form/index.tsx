28c2f0e5eace67b0e11c62a1e4a1c774d726d168
IvanRavinskiy <ivan.ravinskiy@gmail.com>
2022-07-28 14:59:19 +0300

    add registration form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,55 @@
+import {ChangeEvent, FormEvent, useState} from "react";
+import emailjs from '@emailjs/browser';
+
+export const Form = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const changeName = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+
+    const changeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+
+    const changePassword = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
+    const cleanForm = () => {
+        setName('')
+        setEmail('')
+        setPassword('')
+    }
+
+    const sendToEmail = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        emailjs.sendForm('service_ozo4pup', 'template_v4xnbmc', e.currentTarget, '8PHv4YxKhdfQNkxdh')
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+        cleanForm()
+}
+
+    return (
+        <div>
+            <h1>Registration form</h1>
+            <form onSubmit={sendToEmail}>
+                <div>
+                    <label>Name </label>
+                    <input type={'text'} name={'name'} value={name} onChange={changeName}/>
+                </div>
+                <div>
+                    <label>Email </label>
+                    <input type={'email'} name={'email'} value={email} onChange={changeEmail}/>
+                </div>
+                <div>
+                    <label>Password </label>
+                    <input type={'password'} name={'password'} value={password} onChange={changePassword}/>
+                </div>
+                <input type={'submit'} value={'Send'}/>
+            </form>
+        </div>
+    )
+}
